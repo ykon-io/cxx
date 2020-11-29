ffccaf5b6f908e4e94ab5df271d1b62f4f591e14
Maximilian Richter <max.richter@anvajo.com>
2020-11-29 17:50:34 +0100

    initial uuid api draft

--- cxx/uuid.hpp
+++ cxx/uuid.hpp
@@ -1,7 +1,24 @@
+#include "i128.hpp"
+
 namespace cxx 
 {
-    class Uuid
-    {
+    class Uuid {
         
+        public:
+
+        Uuid() {
+        }
+
+        bool operator==(const Uuid & other) {
+            return value == other.value;
+        }
+
+        bool operator!=(const Uuid & other) {
+            return value != other.value;
+        }
+
+        private:
+        
+        i128 value;
     };
 }
