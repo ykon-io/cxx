842ff6ab96334cb28a7c169ac000383af4f36556
Maximilian Richter <max.richter@anvajo.com>
2020-11-29 17:43:47 +0100

    time api concept

--- cxx/time.hpp
+++ cxx/time.hpp
@@ -1,4 +1,5 @@
 #include <ctime>
+#include "f32.hpp"
 
 namespace cxx 
 {
@@ -14,7 +15,7 @@ namespace cxx
             }
 
             static Time epoch();
-            static Time since(int timepoint);
+            static Time since(Time t);
 
             static Time ns(int nanoseconds);
             static Time ms(int milliseconds);
@@ -27,30 +28,49 @@ namespace cxx
             static Time years(int years);
             static Time date(int years, int months, int days, int hours, int minutes, int seconds);
 
-            // getYear()
-            // setYear()
-            // getMonth()
-            // setMonth()
-            // getWeek()
-            // setWeek()
-            // getDay()
-            // setDay()
-            // getHour()
-            // setHour()
-            // getMinute()
-            // setMinute()
-            // getSecond()
-            // setSecond()
-            // getMillisecond()
-            // getNanosecond()
+            f32 ns() const {
+                return 0;
+            }
+            
+            f32 us() const {
+                return 0;
+            }
+            
+            f32 ms() const {
+                return 0;
+            }
+            
+            f32 s() const {
+                return 0;
+            }
 
+            f32 m() const {
+                return 0;
+            }
+
+            f32 h() const {
+                return 0;
+            }
+
+            f32 d() const {
+                return 0;
+            }
+
+            f32 w() const {
+                return 0;
+            }
+
+            f32 y() const {
+                return 0;
+            }
+
+
+            
         // operators
 
             time_t c() const {
                 return value;
             }
-
-            // ... std() const
     
         private:
 
@@ -64,4 +84,7 @@ namespace cxx
     Time operator-(Time left, Time right) {
         return left.c() - right.c();
     }
+
+    void format(const Time & t);
+    void print(const Time & t);
 }
