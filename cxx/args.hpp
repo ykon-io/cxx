2212c08532c2fb15a0b173da9588efc8d61ea4f2
ykon-io <thesoftbear@gmail.com>
2020-11-30 23:47:56 +0100

    update arg access

--- cxx/args.hpp
+++ cxx/args.hpp
@@ -6,8 +6,28 @@ namespace cxx
         int c;
         char ** v;
 
-        int size() { return c; }
-        char* operator[](size_t index) { return v[index]; }
+        int size() { 
+            return c; 
+        }
+
+        char* operator[](size_t index) { 
+            return v[index]; 
+        }
+
+        template<size_t S>
+        bool contains(const char(&name)[S]) const {
+            for(int i = 0; i < c; i++) {
+                if(is(i, name)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        bool is(int i, const char * name) const {
+            return i >= c ? false : strcmp(v[i], name) == 0;
+        }
     };
 
     static arguments arg;
