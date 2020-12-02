f32c3608b786ea37db3ca57c6e40b3e0ff668cd4
ykon-io <thesoftbear@gmail.com>
2020-12-02 19:19:33 +0100

    namespacing

--- build.cpp
+++ build.cpp
@@ -1,49 +1,51 @@
 #include "cxx/build.hpp"
 
-auto s = build::Settings::clang()
-         .o3()
-         .cxx11();
+namespace build {
+    Settings s = Settings::clang()
+                .o3()
+                .cxx11();
 
-build::Library l = {
-    .name = "lib",
-    .sources = {
-        "a.cpp",
-        "b.cpp",
-        build::when {
-            true,
-            "c.cpp"
-            "d.cpp"
+    Library l = {
+        .name = "lib",
+        .sources = {
+            "a.cpp",
+            "b.cpp",
+            when {
+                true,
+                "c.cpp",
+                "d.cpp"
+            }
+        },
+        .include = {
+            "a.hpp",
+            "b.hpp"
+        },
+        .settings = s
+    };
+
+    Executable e = {
+        .name = "exe",
+        .sources = {
+            "exe.cpp"
+        },
+        .objects = {
+            l.objects
         }
-    },
-    .include = {
-        "a.hpp",
-        "b.hpp"
-    },
-    .settings = s
-};
+    };
 
-build::Executable e = {
-    .name = "exe",
-    .sources = {
-        "exe.cpp"
-    },
-    .objects = {
-        l.objects
-    }
-};
-
-build::Package p = { 
-    .name = "package", 
-    .version = "1.0.0",
-    .authors = { 
-        "Author Name <example@mail.com>"
-    },
-    .license = "MIT/Apache-2.0",
-    .readme = "README.md",
-    .keywords = { 
-        "build",
-        "example", 
-        "test"
-    },
-    .description = "stuff"
-};
+    Package p = { 
+        .name = "package", 
+        .version = "1.0.0",
+        .authors = { 
+            "Author Name <example@mail.com>"
+        },
+        .license = "MIT/Apache-2.0",
+        .readme = "README.md",
+        .keywords = { 
+            "build",
+            "example", 
+            "test"
+        },
+        .description = "stuff"
+    };
+}
